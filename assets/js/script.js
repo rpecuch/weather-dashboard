fbab5d315282219a797b19405f27a1b737aa6943
userrp <109092365+rpecuch@users.noreply.github.com>
2022-08-26 17:23:43 -0400

    wrote function to fetch current weather data from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,42 @@ var errorEl = document.querySelector("#error-message");
 
 function handleSubmitForm(event) {
     event.preventDefault();
-    var cityInput = cityInputEl.value
+    var cityInput = cityInputEl.value.toLowerCase();
     if(cityInput === "") {
         errorEl.textContent = "Please type a city name!"
-        //run function to retrieve data
     }
     else {
         errorEl.textContent = "";
+        getWeatherData(cityInput);
     }
 }
 
 submitBtn.addEventListener("click", handleSubmitForm);
+
+function getWeatherData(cityInput) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=69f30a43ab68091abf44ef0a8bf5b7d9&units=imperial"
+    fetch(requestUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                .then(function (data) {
+                    console.log(data);
+                    var currentTemp = data.main.temp;
+                    var currentWind = data.wind.speed;
+                    var currentHumid = data.main.humidity;
+                    displayData(currentTemp, currentWind, currentHumid)
+                    //also need uv index
+                })
+            }
+            else {
+                alert("Error: " + response.statusText);
+            }
+        })
+}
+
+function displayData(currentTemp, currentWind, currentHumid) {
+    console.log(currentTemp);
+    console.log(currentWind);
+    console.log(currentHumid);
+    //now need to display items
+}
