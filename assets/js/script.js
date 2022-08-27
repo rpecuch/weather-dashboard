6b8e61408d4264e5fbc4a9e8f70c4943dc5f285a
userrp <109092365+rpecuch@users.noreply.github.com>
2022-08-27 17:06:25 -0400

    retrieved uv data and displayed results to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,8 @@ function getWeatherData(cityInput) {
                     var weatherIcon = data.weather[0].icon
                     var lat = data.coord.lat;
                     var lon = data.coord.lon;
-                    getUvIndex(lat,lon);
-                    displayData(currentTemp, currentWind, currentHumid, weatherIcon)
-                    //also need uv index
+                    getUvIndex(cityInput, lat,lon);
+                    displayData(cityInput, currentTemp, currentWind, currentHumid, weatherIcon);
                 })
             }
             else {
@@ -41,23 +40,46 @@ function getWeatherData(cityInput) {
         })
 }
 
-function getUvIndex(lat,lon) {
-    var uvUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=69f30a43ab68091abf44ef0a8bf5b7d9";
+function getUvIndex(cityInput, lat,lon) {
+    var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?q=" + cityInput + "&appid=69f30a43ab68091abf44ef0a8bf5b7d9&lat=" + lat + "&lon=" + lon;
     fetch(uvUrl)
         .then(function(response){
             if(response.ok) {
                 response.json()
                 .then(function(data) {
                     console.log(data);
+                    var UvIndex = data.value;
+                    displayUv (UvIndex);
                 })
             }
         })
 }
 
-function displayData(currentTemp, currentWind, currentHumid, weatherIcon) {
+function displayData(cityInput, currentTemp, currentWind, currentHumid, weatherIcon) {
     console.log(currentTemp);
     console.log(currentWind);
     console.log(currentHumid);
     console.log(weatherIcon);
     //now need to display items
+    var cityResult = document.querySelector("#city-result");
+    cityResult.textContent = "City: " + cityInput;//add date
+    var tempResult = document.querySelector("#temp-result");
+    tempResult.textContent = "Temp: " + currentTemp;
+    var windResult = document.querySelector("#wind-result");
+    windResult.textContent = "Wind: " + currentWind;
+    var humidResult = document.querySelector("#humid-result");
+    humidResult.textContent = "Humidity: " + currentHumid;
+    //need to add weather icon
+    //need to add units
 }
+
+function displayUv(UvIndex) {
+    console.log(UvIndex);
+    var UvResult = document.querySelector("#uv-result");
+    UvResult.textContent = "UV Index: " + UvIndex;
+    //need to color code
+}
+
+// now get and display 5-day forecast
+// function getForecast() {
+// }
